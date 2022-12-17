3c45555f0f725b9deceb564c4d40a234d95215e8
fxop0218 <francescoli0218@gmail.com>
2022-12-17 17:48:16 +0100

    Excel export employee information added

--- Models/EmployeeModel.cs
+++ Models/EmployeeModel.cs
@@ -1,10 +1,19 @@
-﻿namespace SalesManagment.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesManagment.Models
 {
     public class EmployeeModel
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(40, ErrorMessage= "The {0} value cannot exceed {1} characters", MinimumLength = 2)]
         public string FirstName { get; set; }
+        [Required]
+        [StringLength(40, ErrorMessage = "The {0} value cannot exceed {1} characters", MinimumLength = 2)]
         public string Surname { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public string Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
